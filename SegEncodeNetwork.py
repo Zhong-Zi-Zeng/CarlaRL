02038eb0d2f63ab49c200bd5b9a93cd54182512e
Zhong-Zi-Zeng <ximen0806449@gmail.com>
2022-04-20 13:15:01 +0800

    Add SegEncodeNetwork.py

---
+++ SegEncodeNetwork.py
@@ -0,0 +1,54 @@
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+
+
+class BlockLayer(Model):
+    def __init__(self,filters,max_flag=False):
+        super().__init__()
+        self.filters = filters
+        self.max_flag = max_flag
+
+        self.conv_la = Conv2D(self.filters, (3, 3), strides=(1,1), padding='same', use_bias=False)
+        self.max_la = MaxPooling2D((2,2))
+        self.bn_la = BatchNormalization()
+        self.ac_la = Activation('relu')
+
+    def call(self,inputs):
+        output = self.conv_la(inputs)
+        if self.max_flag:
+            output = self.max_la(output)
+        output = self.bn_la(output)
+        output = self.ac_la(output)
+
+        return output
+
+class EncodeNetwork(Model):
+    def __init__(self):
+        super().__init__()
+        self.filters_list = [8,16,32,64]
+        self.layerSequential = Sequential()
+
+        for i,filters in enumerate(self.filters_list):
+            for _ in range(2):
+                if i == 0:
+                    layer = BlockLayer(filters,max_flag=True)
+                else:
+                    layer = BlockLayer(filters,max_flag=False)
+                self.layerSequential.add(layer)
+
+    def call(self, inputs):
+        output = self.layerSequential(inputs)
+
+        return output
+
+a = EncodeNetwork()
+a.build(input_shape=(1,200,300,3))
+a.summary()
+
+
